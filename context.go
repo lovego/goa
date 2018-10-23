1ac1a463e0da25600b753e48917c0453f68f1689
bughou <applejava@qq.com>
2018-10-23 16:49:42 +0800

    add server, context methods

--- context.go
+++ context.go
@@ -1,7 +1,15 @@
 package goa
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
+	"reflect"
 )
 
 type Context struct {
@@ -28,4 +36,93 @@ func (c *Context) Next() {
 		return
 	}
 	c.handlers[c.index](c)
+}
+
+func (c *Context) Scheme() string {
+	if proto := c.Request.Header.Get("X-Forwarded-Proto"); proto != `` {
+		return proto
+	}
+	return `http`
+}
+
+func (c *Context) Url() string {
+	return c.Scheme() + `://` + c.Request.Host + c.Request.RequestURI
+}
+
+func (c *Context) Redirect(path string) {
+	c.ResponseWriter.Header().Set("Location", path)
+	c.ResponseWriter.WriteHeader(302)
+}
+
+func (c *Context) Status() int64 {
+	status := reflect.ValueOf(c.ResponseWriter).Elem().FieldByName(`status`)
+	if status.IsValid() {
+		return status.Int()
+	} else {
+		return 0
+	}
+}
+
+func (c *Context) RequestBody() []byte {
+	if c.data == nil {
+		c.data = make(map[string]interface{})
+	}
+	if data, ok := c.data["requestBody"]; ok {
+		if body, ok := data.([]byte); ok {
+			return body
+		}
+		return nil
+	}
+	body, bodyReader := readAndClone(c.Request.Body)
+	c.data["requestBody"] = body
+	c.Request.Body = bodyReader
+
+	return body
+}
+
+func (c *Context) Write(content []byte) (int, error) {
+	if c.data == nil {
+		c.data = make(map[string]interface{})
+	}
+	if data, ok := c.data["responseBody"]; ok {
+		if body, ok := data.([]byte); ok {
+			body = append(body, content...)
+			c.data["responseBody"] = body
+		}
+	}
+	return c.ResponseWriter.Write(content)
+}
+
+func (c *Context) ResponseBody() []byte {
+	if c.data == nil {
+		c.data = make(map[string]interface{})
+	}
+	if data, ok := c.data["responseBody"]; ok {
+		if body, ok := data.([]byte); ok {
+			return body
+		}
+	}
+	return nil
+}
+
+func (c *Context) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if hijacker, ok := c.ResponseWriter.(http.Hijacker); ok {
+		return hijacker.Hijack()
+	}
+	return nil, nil, errors.New("the ResponseWriter doesn't support the Hijacker interface")
+}
+
+func (c *Context) Flush() {
+	if flusher, ok := c.ResponseWriter.(http.Flusher); ok {
+		flusher.Flush()
+	}
+}
+
+func readAndClone(reader io.ReadCloser) ([]byte, io.ReadCloser) {
+	body, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Printf("read http request body error: %v", err)
+		return nil, nil
+	}
+	return body, ioutil.NopCloser(bytes.NewBuffer(body))
 }
---
+++ server/server.go
@@ -0,0 +1,91 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+func init() {
+	if n := runtime.NumCPU() - 1; n >= 1 {
+		runtime.GOMAXPROCS(n)
+	}
+}
+
+type Server struct {
+	*http.Server
+}
+
+func ListenAndServe(handler http.Handler) {
+	s := Server{&http.Server{}}
+	s.ListenAndServe(handler)
+}
+
+func (s Server) ListenAndServe(handler http.Handler) {
+	s.Server.Handler = handler
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGTERM, os.Interrupt)
+
+	go func() {
+		if err := s.Server.Serve(getListener()); err != nil && err != http.ErrServerClosed {
+			log.Panic(err)
+		}
+	}()
+
+	<-ch
+	s.gracefulShutdown()
+}
+
+func (s Server) gracefulShutdown() {
+	if runtime.GOOS != "linux" {
+		return
+	}
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(7*time.Second))
+	defer cancel()
+	if err := s.Server.Shutdown(ctx); err == nil {
+		log.Println(`shutdown`)
+	} else {
+		log.Printf("shutdown error: %v", err)
+	}
+}
+
+func getListener() net.Listener {
+	port := os.Getenv(`GOPORT`)
+	if port == `` {
+		port = `3000`
+	}
+	addr := `:` + port
+	ln, err := net.Listen(`tcp`, addr)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println(color.GreenString(`started.(` + addr + `)`))
+	return tcpKeepAliveListener{ln.(*net.TCPListener)}
+}
+
+// tcpKeepAliveListener sets TCP keep-alive timeouts on accepted
+// connections. It's used by ListenAndServe and ListenAndServeTLS so
+// dead TCP connections (e.g. closing laptop mid-download) eventually
+// go away.
+type tcpKeepAliveListener struct {
+	*net.TCPListener
+}
+
+func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return
+	}
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(3 * time.Minute)
+	return tc, nil
+}
