2b4497a57cf329cffcbdf7779380af38228f4f61
bughou <bughou@gmail.com>
2021-11-02 14:37:17 +0800

    fix cors when same-origin

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -17,25 +17,27 @@ func NewCORS(allow func(origin string) bool) CORS {
 }
 
 func (cors CORS) Check(c *goa.Context) {
-	if origin := c.Request.Header.Get(`Origin`); origin != `` && origin != c.Origin() {
-		if !cors.allow(origin) {
-			c.WriteHeader(http.StatusForbidden)
-			c.Write([]byte(`origin not allowed.`))
-			return
-		}
-		header := c.ResponseWriter.Header()
-		header.Set(`Access-Control-Allow-Origin`, origin)
-		header.Set(`Access-Control-Allow-Credentials`, `true`)
-		header.Set(`Vary`, `Accept-Encoding, Origin`)
-
-		if c.Request.Method == `OPTIONS` { // preflight request
-			header.Set(`Access-Control-Max-Age`, `86400`)
-			header.Set(`Access-Control-Allow-Methods`, `GET, POST, PUT, DELETE, PATCH`)
-			header.Set(`Access-Control-Allow-Headers`, `X-Requested-With, Content-Type, withCredentials`)
-			if cors.SetHeader != nil {
-				cors.SetHeader(header)
+	if c.Request.Header.Get(`Sec-Fetch-Site`) != "same-origin" {
+		if origin := c.Request.Header.Get(`Origin`); origin != `` && origin != c.Origin() {
+			if !cors.allow(origin) {
+				c.WriteHeader(http.StatusForbidden)
+				c.Write([]byte(`origin not allowed.`))
+				return
 			}
-			return
+			header := c.ResponseWriter.Header()
+			header.Set(`Access-Control-Allow-Origin`, origin)
+			header.Set(`Access-Control-Allow-Credentials`, `true`)
+			header.Set(`Vary`, `Accept-Encoding, Origin`)
+
+			if c.Request.Method == `OPTIONS` { // preflight request
+				header.Set(`Access-Control-Max-Age`, `86400`)
+				header.Set(`Access-Control-Allow-Methods`, `GET, POST, PUT, DELETE, PATCH`)
+				header.Set(`Access-Control-Allow-Headers`, `X-Requested-With, Content-Type, withCredentials`)
+				if cors.SetHeader != nil {
+					cors.SetHeader(header)
+				}
+				return
+			}
 		}
 	}
 
