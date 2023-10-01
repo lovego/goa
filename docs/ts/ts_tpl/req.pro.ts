22f2448cd8b8c1f4d56b300b90363583110d1b97
李春华 <lichunhua@threesoft.cn>
2023-10-01 14:32:37 +0800

    fix

--- docs/ts/ts_tpl/req.pro.ts
+++ docs/ts/ts_tpl/req.pro.ts
@@ -11,22 +11,17 @@ import request from "@/utils/request";
 
 ≤if !IsZero(.Header) ≥≤"\n"≥
 // 请求头描述
-/**
-≤range  index, field := .Header.Members≥ * @params ≤field.JsonName≥:≤if field.Comment != "" ≥ ≤field.Comment≥≤end≥≤if len(.Header.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
- */
-export type header = {
-≤range  index, field := .Header.Members≥    ≤field.JsonName≥?: ≤field.Type≥;≤if field.Comment != "" ≥ // ≤field.Comment≥≤end≥≤if len(.Header.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
+≤range  index, field := .Header.Members≥    ≤if field.Comment != "" ≥/*≤field.Comment≥*/≤end≥
+    ≤field.JsonName≥?: ≤field.Type≥;≤if len(.Header.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
 };
 
 ≤end≥
 
 
 ≤if !IsZero(.Param) ≥≤"\n"≥
-/**
-≤range  index, field := .Param.Members≥ * @params ≤field.JsonName≥:≤if field.Comment != "" ≥ ≤field.Comment≥≤end≥≤if len(.Param.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
- */
 export type paramReq = {
-≤range  index, field := .Param.Members≥    ≤field.JsonName≥: ≤field.Type≥;≤if field.Comment != "" ≥ // ≤field.Comment≥≤end≥≤if len(.Param.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
+≤range  index, field := .Param.Members≥    ≤if field.Comment != "" ≥/*≤field.Comment≥*/ ≤end≥
+    ≤field.JsonName≥: ≤field.Type≥;≤if len(.Param.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
 };
 
 ≤end≥
@@ -34,44 +29,36 @@ export type paramReq = {
 
 
 ≤range  index, api := .TypeList≥
-/**
-≤range  index, field := api.Members≥ * @params ≤field.JsonName≥:≤if field.Comment != "" ≥ ≤field.Comment≥≤end≥≤if len(api.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
- */
 export type ≤api.Name≥ = {
-≤range  index, field := api.Members≥    ≤field.JsonName≥: ≤field.Type≥;≤if field.Comment != "" ≥ // ≤field.Comment≥≤end≥≤if len(api.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
+≤range  index, field := api.Members≥    ≤if field.Comment != "" ≥/*≤field.Comment≥*/ ≤end≥
+    ≤field.JsonName≥: ≤field.Type≥;≤if len(api.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
 };
 ≤end≥
 
 
 
 ≤if !IsZero(.Query) ≥≤"\n"≥
-/**
-≤range  index, field := .Query.Members≥ * @params ≤field.JsonName≥:≤if field.Comment != "" ≥ ≤field.Comment≥≤end≥≤if len(.Query.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
- */
 export type queryReq = {
-≤range  index, field := .Query.Members≥    ≤field.JsonName≥?: ≤field.Type≥;≤if field.Comment != "" ≥ // ≤field.Comment≥≤end≥≤if len(.Query.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
+≤range  index, field := .Query.Members≥    ≤if field.Comment != "" ≥/*≤field.Comment≥*/ ≤end≥
+    ≤field.JsonName≥?: ≤field.Type≥;≤if len(.Query.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
 };
 
 ≤end≥
 
 
 ≤if !IsZero(.Body) ≥≤"\n"≥
-/**
-≤range  index, field := .Body.Members≥ * @params ≤field.JsonName≥:≤if field.Comment != "" ≥ ≤field.Comment≥≤end≥≤if len(.Body.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
- */
 export type bodyReq = {
-≤range  index, field := .Body.Members≥    ≤field.JsonName≥?: ≤field.Type≥;≤if field.Comment != "" ≥ // ≤field.Comment≥≤end≥≤if len(.Body.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
+≤range  index, field := .Body.Members≥    ≤if field.Comment != "" ≥/*≤field.Comment≥*/ ≤end≥
+    ≤field.JsonName≥?: ≤field.Type≥;≤if len(.Body.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
 };
 
 ≤end≥
 
 
 ≤if !IsZero(.Resp) ≥≤"\n"≥
-/**
-≤range  index, field := .Resp.Members≥ * @params ≤field.JsonName≥:≤if field.Comment != "" ≥ ≤field.Comment≥≤end≥≤if len(.Resp.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
- */
 export type resp = {
-≤range  index, field := .Resp.Members≥    ≤field.JsonName≥?: ≤field.Type≥;≤if field.Comment != "" ≥ // ≤field.Comment≥≤end≥≤if len(.Resp.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
+≤range  index, field := .Resp.Members≥    ≤if field.Comment != "" ≥/*≤field.Comment≥*/ ≤end≥
+    ≤field.JsonName≥?: ≤field.Type≥;≤if len(.Resp.Members) > index + 1 ≥≤"\n"≥≤end≥≤end≥
 };
 
 ≤end≥
